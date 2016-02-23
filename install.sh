7549de27f24cfe5c1f5091870e13183956ed6d4b
Dmitriy Khramtsov <highdn@gmail.com>
2016-02-23 05:14:27 +0000

    Add install.sh script to quick-setup new virtual machine.

---
+++ install.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#
+# To execute this file on a newly created VM run:
+#
+# $ sudo apt-get install git
+# $ git clone https://github.com/highdn/highdn-com.git
+# $ ./highdn-com/install.sh
+#
+
+set -o errexit
+set -o xtrace
+
+sudo apt-get install npm nodejs nodejs-legacy unzip
+
+sudo npm install -g bower
+
+# Get latest download URL of Google App Engine SDK for Go at
+# https://cloud.google.com/appengine/downloads#Google_App_Engine_SDK_for_Go
+
+wget https://storage.googleapis.com/appengine-sdks/featured/go_appengine_sdk_linux_amd64-1.9.33.zip
+unzip go_appengine_sdk_linux_amd64-1.9.33.zip
+
+echo "Add $PWD/go_appengine to your search paths."
+PATH="$PWD/go_appengine:$PATH"
+
+# Provide Google account credentials to access Google App Engine.
+appcfg.py list_versions $(dirname $0) --noauth_local_webserver
+
+echo SUCCESS.
